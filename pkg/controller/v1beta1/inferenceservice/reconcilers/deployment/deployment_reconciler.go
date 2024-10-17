6abba54b603c1d7aba95a7202fc3c979c31a6309
Vedant Mahabaleshwarkar <vmahabal@redhat.com>
2024-10-17 12:56:13 -0400

    add oauth proxy flag to prevent login page redirect on invalid request
    Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

--- pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
+++ pkg/controller/v1beta1/inferenceservice/reconcilers/deployment/deployment_reconciler.go
@@ -258,6 +258,7 @@ func generateOauthProxyContainer(clientset kubernetes.Interface, isvc string, na
 		Args: []string{
 			`--https-address=:8443`,
 			`--provider=openshift`,
+			`--skip-provider-button`,
 			`--openshift-service-account=kserve-sa`,
 			`--upstream=http://localhost:` + upstreamPort,
 			`--tls-cert=/etc/tls/private/tls.crt`,
