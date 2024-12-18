723b541ca37c8f3671d1149cd6c7b9ce8815e00e
Hannah DeFazio <h2defazio@gmail.com>
2024-12-18 15:15:43 -0500

    debug

--- test/scripts/gh-actions/run-e2e-tests.sh
+++ test/scripts/gh-actions/run-e2e-tests.sh
@@ -30,5 +30,5 @@ fi
 PARALLELISM="${2:-1}"
 source python/kserve/.venv/bin/activate
 pushd test/e2e >/dev/null
-  pytest -m "$1" --ignore=qpext --log-cli-level=INFO -n $PARALLELISM --dist worksteal
+  pytest -m "$1" --ignore=qpext --log-cli-level=DEBUG -n $PARALLELISM --dist worksteal
 popd
