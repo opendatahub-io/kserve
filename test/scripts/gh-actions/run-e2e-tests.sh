4ca28c7d23920eb72e6b737a02b974625ec13a83
Hannah DeFazio <h2defazio@gmail.com>
2024-12-19 14:53:04 -0500

    tell pytest not to capture output

--- test/scripts/gh-actions/run-e2e-tests.sh
+++ test/scripts/gh-actions/run-e2e-tests.sh
@@ -31,6 +31,6 @@ PARALLELISM="${2:-1}"
 source python/kserve/.venv/bin/activate
 pushd test/e2e >/dev/null
   pytest --collect-only
-  pytest -v --full-trace --setup-show -m "$1" --ignore=qpext --log-cli-level=DEBUG -n $PARALLELISM --dist worksteal
+  pytest -v -s --full-trace --setup-show -m "$1" --ignore=qpext --log-cli-level=DEBUG -n $PARALLELISM --dist worksteal
 popd
 echo "Done!"
