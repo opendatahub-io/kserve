e23833d3e4564143778930df6824396c639c29ec
Vedant Mahabaleshwarkar <vmahabal@redhat.com>
2025-06-27 12:58:17 -0400

    add namespace to oc exec while fetching certs
    Signed-off-by: Vedant Mahabaleshwarkar <vmahabal@redhat.com>

--- test/scripts/openshift-ci/setup-e2e-tests.sh
+++ test/scripts/openshift-ci/setup-e2e-tests.sh
@@ -124,7 +124,7 @@ kustomize build $PROJECT_ROOT/test/scripts/openshift-ci |
 # Configure certs for the python requests by getting the CA cert from the kserve controller pod 
 export CA_CERT_PATH="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 # The run-e2e-tests script expects the CA cert to be in /tmp/ca.crt
-oc exec deploy/kserve-controller-manager -- cat $CA_CERT_PATH > /tmp/ca.crt
+oc exec deploy/kserve-controller-manager -n kserve -- cat $CA_CERT_PATH > /tmp/ca.crt
 
 echo "Add testing models to minio storage ..." # Reference: config/overlays/test/minio/minio-init-job.yaml
 
