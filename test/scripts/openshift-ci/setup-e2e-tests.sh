ce482bbc344f31fc76f5ed0b2f47bb787f0482ed
Spolti <fspolti@redhat.com>
2025-07-17 14:59:35 -0300

    ModuleNotFoundError: No module named timeout_sampler
    chore:	test_multi_container_probing.py needs the timeout-sampler package.
    	After the migration to uv it is not available anymore.
    
    Signed-off-by: Spolti <fspolti@redhat.com>

--- test/scripts/openshift-ci/setup-e2e-tests.sh
+++ test/scripts/openshift-ci/setup-e2e-tests.sh
@@ -65,6 +65,7 @@ popd
 echo "Installing KServe Python SDK ..."
 pushd $PROJECT_ROOT/python/kserve >/dev/null
   uv sync --active --group test
+  uv pip insall timeout-sampler
 popd
 
 $MY_PATH/deploy.cma.sh
