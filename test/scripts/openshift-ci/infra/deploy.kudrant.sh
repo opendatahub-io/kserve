908402d517782b657d72ddc92bce345500e927bb
Jooho Lee <jlee@redhat.com>
2025-09-02 14:58:17 -0400

    add install scripts(kuadrant/gateway api) and setup for llm
    Signed-off-by: Jooho Lee <jlee@redhat.com>

---
+++ test/scripts/openshift-ci/infra/deploy.kudrant.sh
@@ -0,0 +1,47 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../common.sh"
+KUADRANT_NS="kuadrant-system"
+
+echo "⏳ Installing RHCL(Kuadrant) operator"
+oc create ns ${KUADRANT_NS} || true
+
+{
+cat <<EOF | oc create -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: rhcl-operator
+  namespace: kuadrant-system
+spec:
+  channel: stable
+  installPlanApproval: Automatic
+  name: rhcl-operator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+---
+kind: OperatorGroup
+apiVersion: operators.coreos.com/v1
+metadata:
+  name: kuadrant
+  namespace: ${KUADRANT_NS}
+spec:
+  upgradeStrategy: Default
+EOF
+} || true
+
+wait_for_crd  kuadrants.kuadrant.io  90s
+
+{
+cat <<EOF | oc create -f -
+apiVersion: kuadrant.io/v1beta1
+kind: Kuadrant
+metadata:
+  name: kuadrant
+  namespace: ${KUADRANT_NS}
+EOF
+} || true
+
+echo "⏳ waiting for authorino-operator to be ready.…"
+wait_for_pod_ready "${KUADRANT_NS}" "authorino-resource=authorino"
+
+echo "✅ kuadrant(authorino) installed"
---
+++ test/scripts/openshift-ci/setup-llm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/common.sh"
+PROJECT_ROOT="$(find_project_root "$SCRIPT_DIR")"
+
+# Get OpenShift server version and execute appropriate script
+server_version=$(get_openshift_server_version)
+echo "Checking OpenShift server version...($server_version)"
+
+# Execute script based on version comparison
+if version_compare "$server_version" "4.19.0"; then
+  echo "🎯 Server version ($server_version) is 4.19.0 or higher - continue with the script"
+else
+  echo "🎯 Server version ($server_version) is not supported so stop the script"
+  exit 1
+fi
+
+# Installing Cert Manager
+$SCRIPT_DIR/infra/deploy.cert-manager.sh
+
+# Installing LWS Operator" 
+$SCRIPT_DIR/infra/deploy.lws.sh
+
+# Installing Gateway Ingress
+$SCRIPT_DIR/infra/deploy.gateway.ingress.sh
+
+# Installing RHCL(Kuadrant) operator
+$SCRIPT_DIR/infra/deploy.kudrant.sh
+
+# Installing KServe
+kubectl create ns opendatahub || true
+
+kubectl kustomize config/crd/ | kubectl apply --server-side=true -f -
+wait_for_crd  llminferenceserviceconfigs.serving.kserve.io  90s
+
+kustomize build config/overlays/odh | kubectl apply  --server-side=true --force-conflicts -f -
+wait_for_pod_ready "opendatahub" "control-plane=kserve-controller-manager" 300s
+
