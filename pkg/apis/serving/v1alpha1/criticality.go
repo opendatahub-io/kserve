45234e4a3339b6869ca36a665ded9fd0ef2f0bc0
Killian Golds <kgolds@redhat.com>
2025-10-06 17:27:53 +0100

    Apply fork workaround for GIE v1.0.0 validation bug and regenerate manifests
    Gateway API Inference Extension v1.0.0 has invalid kubebuilder validation
    markers (MinItems/MaxItems on a map field) causing controller-gen to fail.
    Applied temporary fork workaround pointing to personal fork based on v1.0.0
    tag with the fix applied.
    
    Changes:
    - Add replace directive in go.mod for GIE fork at kserve-compatibility-v1.0
    - Regenerate all CRD manifests with controller-gen v0.17.2
    - Regenerate Go code (deepcopy, openapi) and Python SDK
    - Fix linting errors in scheduler.go
    
    The fork maintains K8s v0.33.4 compatibility (no version conflicts).
    This is a temporary workaround until upstream merges the fix and releases
    a patched version.
    
    Upstream issue: https://github.com/kubernetes-sigs/gateway-api-inference-extension/issues/1678
    Upstream fix PR: https://github.com/kubernetes-sigs/gateway-api-inference-extension/pull/1679
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- pkg/apis/serving/v1alpha1/criticality.go
+++ pkg/apis/serving/v1alpha1/criticality.go
@@ -1,6 +1,22 @@
+/*
+Copyright 2023 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
-//This has been extracted as v1 does not utilise criticality, but v1alpha2 does.
+// This has been extracted as v1 does not utilise criticality, but v1alpha2 does.
 
 // ModelCriticality expresses the relative importance of serving a model.
 // This is used by our scheduler integration and stays in KServe domain.
