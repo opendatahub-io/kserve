dcdeee7d783a0a8e09c0ef810c44b7b4e324fa56
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-10-20 18:38:44 +0200

    Revert test/scripts/gh-actions/run-e2e-tests.sh
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/scripts/gh-actions/run-e2e-tests.sh
+++ test/scripts/gh-actions/run-e2e-tests.sh
@@ -42,7 +42,7 @@ source python/kserve/.venv/bin/activate
 pushd test/e2e >/dev/null
   if [[ $MARKER == "raw" && $NETWORK_LAYER == "istio-ingress" ]]; then
     echo "Skipping explainer tests for raw deployment with ingress"
-    pytest -m "$MARKER" --exitfirst --ignore=qpext --log-cli-level=INFO -n $PARALLELISM --dist worksteal --network-layer $NETWORK_LAYER --ignore=explainer/
+    pytest --capture=tee-sys -m "$MARKER" --ignore=qpext --log-cli-level=INFO -n $PARALLELISM --dist worksteal --network-layer $NETWORK_LAYER --ignore=explainer/
   else
     rc=0
     pytest --capture=tee-sys -m "$MARKER" --ignore=qpext --log-cli-level=INFO -n $PARALLELISM --dist worksteal --network-layer $NETWORK_LAYER || rc=$?
