c2bd34ee7654a7475e6e3772d5ec2ed23cba7e65
Pierangelo Di Pilato <pierdipi@redhat.com>
2026-03-21 08:35:26 +0100

    opendatahub -> kserve
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/scripts/openshift-ci/run-e2e-tests.sh
+++ test/scripts/openshift-ci/run-e2e-tests.sh
@@ -27,6 +27,7 @@ export INFERENCE_POOL_GROUP="${INFERENCE_POOL_GROUP:-inference.networking.x-k8s.
 export RUN_AS_NON_ROOT="${RUN_AS_NON_ROOT:-true}"
 export SKIP_DELETION_ON_FAILURE=${SKIP_DELETION_ON_FAILURE:-true}
 export KUBE_CLI=${KUBE_CLI_COMMAND:-oc}
+export KSERVE_NAMESPACE=${KSERVE_NAMESPACE:-"kserve"}
 
 MY_PATH=$(dirname "$0")
 PROJECT_ROOT=$MY_PATH/../../../
