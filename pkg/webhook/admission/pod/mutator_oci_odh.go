3fd9e81394af3ed9c072db309705456891545df9
Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
2026-06-18 13:26:10 -0400

    fixing non-distro build by moving InjectImageVolume webhook invokation to distro file
    Signed-off-by: Brett Thompson <196701379+brettmthompson@users.noreply.github.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

---
+++ pkg/webhook/admission/pod/mutator_oci_odh.go
@@ -0,0 +1,38 @@
+//go:build distro
+
+/*
+Copyright 2026 The KServe Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pod
+
+import (
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/kserve/kserve/pkg/constants"
+	"github.com/kserve/kserve/pkg/runtime"
+)
+
+// getOciStorageMutator returns the appropriate mutator for OCI storage based on the runtime type.
+// In distro builds, MLServer runtime uses image volumes while others use modelcar.
+func getOciStorageMutator(pod *corev1.Pod, storageInitializer *StorageInitializerInjector) func(*corev1.Pod) error {
+	// Read server type from pod annotation (propagated from ServingRuntime during component reconciliation)
+	serverType := runtime.GetServerTypeFromPod(pod)
+
+	if serverType == constants.ServerTypeMLServer {
+		return storageInitializer.InjectImageVolume
+	}
+	return storageInitializer.InjectModelcar
+}
